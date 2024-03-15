78fd8dfee74b3d3a9e0cecd54c2c190d8657b8e9
aleks <alekseev.i260303@gmail.com>
2024-03-15 22:03:55 +0300

    Добавлен анализ ГСЧ

--- Анализ генератора.py
+++ Анализ генератора.py
@@ -8,7 +8,12 @@ with open('result.txt', 'r') as result_file:
         result.append(float(result_file.readline()))
 
 g = [result.count(e) for e in set(result)]
-p = [g[i] / len(result) for i in range(len(g))]
+p = [(1 / 10000) * (1 / g[i]) for i in range(len(g))]
+
+p = list(set(p))
+g = list(set(g))
+g.sort()
+p.sort()
 
 M = 0
 for i in range(len(g)):
@@ -19,6 +24,8 @@ Mg2 = 0
 
 for i in range(len(g)):
     Mg2 += (g[i] ** 2) * p[i]
+
+print(M, Mg2)
 
 D = Mg2 - M ** 2
 x = sqrt(D)
@@ -31,12 +38,9 @@ print(f'Максимальное значение: {max(result)}')
 print(f'Уникальных значений в выборке: {len(set(result))}/{len(result)}')
 print(f'Наиболее встречаемое число: {result[g.index(max(g))]}, {max(g)} раз')
 
-left = sorted(p[:len(p) // 2])
-right = sorted(p[len(p) // 2:])
-right.reverse()
+fig, ax = plt.subplots(figsize=(12, 7))
 
-fig, ax = plt.subplots(figsize=(12, 6))
-ax.hist(g, range=(0, 5), weights=p, bins=range(0, 10, 1))
-ax.set_title('Распределение')
+ax.hist(g, weights=p, bins=range(0, len(g), 1))
+ax.set_title(f'Распределение')
 plt.show()
 
